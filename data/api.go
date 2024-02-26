7ae2bb7f3f5214f6c7358b2a57f3d817f9ac9856
zmhuanf <zmhuanf@163.com>
2024-02-26 15:13:38 +0800

    修复数据来源网站证书过期导致的无法请求问题；

--- data/api.go
+++ data/api.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -12,8 +13,19 @@ const api = `https://api.doctorxiong.club/v1/fund/detail/list`
 
 // GetOriginalData 获取原始数据
 func GetOriginalData(code string) ([]byte, bool) {
+	// TODO 2024年临时补丁，用于解决数据来源网站证书过期问题
+	// 创建一个自定义的http.Transport，忽略TLS证书验证
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	// 创建一个使用自定义Transport的http.Client
+	client := &http.Client{
+		Transport: transport,
+	}
 	url := fmt.Sprintf(`%v?code=%v`, api, code)
-	resp, err := http.Get(url)
+	resp, err := client.Get(url)
 	if err != nil {
 		log.Printf(`http请求失败，err：%v，url：%v`, err, url)
 		return nil, false
@@ -25,6 +37,21 @@ func GetOriginalData(code string) ([]byte, bool) {
 		return nil, false
 	}
 	return data, true
+
+	// TODO 补丁前代码
+	//url := fmt.Sprintf(`%v?code=%v`, api, code)
+	//resp, err := http.Get(url)
+	//if err != nil {
+	//	log.Printf(`http请求失败，err：%v，url：%v`, err, url)
+	//	return nil, false
+	//}
+	//defer resp.Body.Close()
+	//data, err := io.ReadAll(resp.Body)
+	//if err != nil {
+	//	log.Printf(`流读取失败，err：%v`, err)
+	//	return nil, false
+	//}
+	//return data, true
 }
 
 // GetData 获取解析后的数据
